4127ee6ad3fc1c8aeead4639d32ed06524ae48fa
hetywong <95894188+hetywong@users.noreply.github.com>
2021-12-22 12:18:39 +0800

    Tour to the YST Conservatory
    A brief introduction to the Music Conservatory of NUS

---
+++ app-files/data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-alice-lee-plaza",
+      "name": "Alice Lee Plaza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.927071751170754,
+        "pitch": -0.21253555308984673,
+        "fov": 1.3123694188512842
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5515883915994717,
+          "pitch": -0.21222747909647666,
+          "rotation": 1.5707963267948966,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.927071751170754,
+          "pitch": -0.21253555308984673,
+          "title": "Yong Siew Toh Conservatory of Music",
+          "text": "<span style=\"color: rgb(34, 34, 34); font-family: Arial, Helvetica, sans-serif; font-size: small; background-color: rgb(255, 255, 255);\">Officially opened in 2006,&nbsp;</span><span style=\"color: rgb(34, 34, 34); font-family: Arial, Helvetica, sans-serif; font-size: small; background-color: rgb(255, 255, 255);\">the YST Conservatory building houses&nbsp;</span><span style=\"color: rgb(34, 34, 34); font-family: Arial, Helvetica, sans-serif; font-size: small; background-color: rgb(255, 255, 255);\">state-of-the-art facilities for training&nbsp;</span><span style=\"color: rgb(34, 34, 34); font-family: Arial, Helvetica, sans-serif; font-size: small; background-color: rgb(255, 255, 255);\">professional musicians.</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-conservatory-foyer",
+      "name": "Conservatory Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.24815643827520262,
+        "pitch": -0.3430542125261091,
+        "fov": 1.3123694188512842
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5765428986942283,
+          "pitch": 0.11430367251000284,
+          "rotation": 7.853981633974483,
+          "target": "2-conservatory-lounge"
+        },
+        {
+          "yaw": -0.6525244305264231,
+          "pitch": 0.07757370466325852,
+          "rotation": 0,
+          "target": "0-alice-lee-plaza"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2482169565860843,
+          "pitch": -0.15148450216687692,
+          "title": "Conservatory Foyer",
+          "text": "<span style=\"color: rgb(34, 34, 34); font-family: Arial, Helvetica, sans-serif; font-size: small; background-color: rgb(255, 255, 255);\">The Foyer space is suitable for both informal (standing) and formal (sit-down) receptions.</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-conservatory-lounge",
+      "name": "Conservatory Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.9836153343172924,
+        "pitch": 0.02081124125776057,
+        "fov": 1.3123694188512842
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3478993340421468,
+          "pitch": 0.19775997760526565,
+          "rotation": 12.566370614359176,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8702046559220857,
+          "pitch": -0.45469631744887096,
+          "title": "Conservatory Lounge",
+          "text": "<span style=\"color: rgb(34, 34, 34); font-family: Arial, Helvetica, sans-serif; font-size: small; background-color: rgb(255, 255, 255);\">The Conservatory Lounge is a comfortable space that invites visitors to sit down, relax and take in their surroundings. It houses the A Tempo café, which sells a selection of food and beverages in the daytime on weekdays, and is open after hours during selected events.</span>"
+        }
+      ]
+    }
+  ],
+  "name": "Yong Siew Toh Conservatory of Music",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
